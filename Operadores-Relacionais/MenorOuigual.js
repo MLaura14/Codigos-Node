747029df741f581620c78413f6c6aaedce413130
Maria Laura <mlsilmeida@gmail.com>
2026-02-26 20:04:39 -0300

    Create MenorOuigual.js

---
+++ Operadores-Relacionais/MenorOuigual.js
@@ -0,0 +1,8 @@
+// o codigo abaixo mostrar se o aluno precisa melhorar ou nao depedendo de sua nota.
+let nota = 6;
+
+if (nota <= 5) {
+    console.log("Precisa melhorar");
+} else {
+    console.log("Nota suficiente");
+}
