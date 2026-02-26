bdec426230abe785afc9f9a17dbb791979e9eda3
Maria Laura <mlsilmeida@gmail.com>
2026-02-26 20:02:51 -0300

    Create diferente.js

---
+++ Operadores-Relacionais/diferente.js
@@ -0,0 +1,8 @@
+//O código compara a idade com 18 e mostra uma mensagem dizendo se ela é diferente ou igual a 18.
+let idade = 16;
+
+if (idade != 18) {
+    console.log("A idade é diferente de 18");
+} else {
+    console.log("A idade é 18");
+}
