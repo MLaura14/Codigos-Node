bb3a95f6fa5bfbea964eefa835fa9bd0e448a73e
Maria Laura <mlsilmeida@gmail.com>
2026-02-26 20:04:12 -0300

    Create menor.js

---
+++ Operadores-Relacionais/menor.js
@@ -0,0 +1,8 @@
+//O código abaixo mostra se a nota é menor que 6 Se for, o aluno está reprovado,se nao, está aprovado.
+let nota = 4;
+
+if (nota < 6) {
+    console.log("Reprovado");
+} else {
+    console.log("Aprovado");
+}
