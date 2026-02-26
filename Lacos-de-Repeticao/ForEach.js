25fb69f367051dfa19eebc1dd8075e720e30dcd1
Maria Laura <mlsilmeida@gmail.com>
2026-02-26 19:54:32 -0300

    Create ForEach.js

---
+++ Lacos-de-Repeticao/ForEach.js
@@ -0,0 +1,6 @@
+//o codigo abaixo é um vetor e o ForEach passa por todos os nomes da lista e executa o console.log
+let nomes = ["Ana", "Carlos", "João"];
+
+nomes.forEach((nome) => {
+    console.log(nome);
+});
