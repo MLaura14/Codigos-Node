77d40a1e576f4c6353de51f81cf7ac20d82c4219
Maria Laura <mlsilmeida@gmail.com>
2026-02-26 19:51:02 -0300

    Create const1.js

---
+++ Declaracao-de-Variaveis/const1.js
@@ -0,0 +1,8 @@
+//o codigo abaixo cria variáveis para armazenar notas e o programa calcula a média.
+const nota1 = 7.5;
+const nota2 = 8.0;
+const nota3 = 6.5;
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+console.log("Média do aluno: " + media);
