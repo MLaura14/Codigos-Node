6fcbe802fa01bd9019a6f9b02581e0b6527dab03
isabellyBatista88 <isabelly.batista747@gmail.com>
2026-02-26 20:54:12 -0300

    Create string.js

---
+++ Declaracao-de-Variaveis/string.js
@@ -0,0 +1,5 @@
+//o codigo abaixo cria duas variáveis e usa a concatenação para montar uma frase completa com essas informações
+const nome = "Ana";
+const idade = 18;
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
