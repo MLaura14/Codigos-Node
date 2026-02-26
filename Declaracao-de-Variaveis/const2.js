009bef99118fb001fa9729c333bb75163e354f73
Maria Laura <mlsilmeida@gmail.com>
2026-02-26 19:51:32 -0300

    Create const2.js

---
+++ Declaracao-de-Variaveis/const2.js
@@ -0,0 +1,8 @@
+//o codigo abaixo Usa variáveis para guardar informações de uma pessoa
+const nome = "João";
+const idade = 17;
+const altura = 1.70;
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade + " anos");
+console.log("Altura: " + altura + " m");
