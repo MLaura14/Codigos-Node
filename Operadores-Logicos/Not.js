5a1a1d481c559b807a208b931eaf68ec23d11314
Maria Laura <mlsilmeida@gmail.com>
2026-02-26 20:01:15 -0300

    Create Not.js

---
+++ Operadores-Logicos/Not.js
@@ -0,0 +1,8 @@
+//o codiigo abaixo verifica se a pessoa não tem carteira para decidir se pode dirigir ou não. 
+let temCarteira = true;
+//O ! serve para inverter a lógica.
+if (!temCarteira) {
+    console.log("Não pode dirigir");
+} else {
+    console.log("Pode dirigir");
+}
